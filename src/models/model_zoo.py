0443bea9f90e102fd23b902f4bb0b877cb732111
Rohan Sukumaran <rohansukumaran@Rohans-MacBook-Pro.local>
2021-09-10 07:03:36 +0530

    removed redundant code

--- src/models/model_zoo.py
+++ src/models/model_zoo.py
@@ -40,30 +40,23 @@ class Model(nn.Module):
             if config["model_name"] == "resnet18":
                 pretrained = config["pretrained"]
                 model = models.resnet18(pretrained=pretrained)
-                num_ftrs = model.fc.in_features
-                model.fc = nn.Sequential(nn.Flatten(),
-                                        nn.Linear(num_ftrs, logits))
-                model = nn.ModuleList(list(model.children())[self.split_layer:])
-                self.model = nn.Sequential(*model)
+                
             elif config["model_name"] == "resnet34":
                 pretrained = config["pretrained"]
                 model = models.resnet34(pretrained=pretrained)
-                num_ftrs = model.fc.in_features
-                model.fc = nn.Sequential(nn.Flatten(),
-                                        nn.Linear(num_ftrs, logits))
-                model = nn.ModuleList(list(model.children())[self.split_layer:])
-                self.model = nn.Sequential(*model)
+                
             elif config["model_name"] == "resnet50":
                 pretrained = config["pretrained"]
                 model = models.resnet50(pretrained=pretrained)
+                
+            else:
+                pass  
+            if config["model_name"].startswith("resnet"):
                 num_ftrs = model.fc.in_features
                 model.fc = nn.Sequential(nn.Flatten(),
                                         nn.Linear(num_ftrs, logits))
                 model = nn.ModuleList(list(model.children())[self.split_layer:])
                 self.model = nn.Sequential(*model)
-            else:
-                pass  
-
 
 
         self.model = self.utils.model_on_gpus(self.model)
